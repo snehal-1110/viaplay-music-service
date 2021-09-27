523eafe4988d40209149ac9709057f63676ceb23
snehal-1110 <snehal.b11@gmail.com>
2021-09-28 01:49:55 +0530

    Initial Config..

---
+++ src/main/java/com/assignment/viaplaymusicservice/ViaplayMusicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.viaplaymusicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaplayMusicServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ViaplayMusicServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/assignment/viaplaymusicservice/ViaplayMusicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.viaplaymusicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaplayMusicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
