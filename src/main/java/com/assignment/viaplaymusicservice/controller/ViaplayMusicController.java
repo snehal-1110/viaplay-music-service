300d936a4b7acef5c17b6be440ec03535a559a4c
snehal-1110 <snehal.b11@gmail.com>
2021-10-02 23:18:33 +0530

    Update ViaplayMusicController.java

--- src/main/java/com/assignment/viaplaymusicservice/controller/ViaplayMusicController.java
+++ src/main/java/com/assignment/viaplaymusicservice/controller/ViaplayMusicController.java
@@ -5,8 +5,8 @@ import com.assignment.viaplaymusicservice.service.ViaplayAlbumService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,7 +20,7 @@ public class ViaplayMusicController {
     }
 
     @GetMapping(value = "/album/{mbId}")
-    public ResponseEntity<ArtistDetailsResponse> getAlbumInfo(@RequestParam(value = "mbId") String mbId) {
+    public ResponseEntity<ArtistDetailsResponse> getAlbumInfo(@PathVariable String mbId) {
         ArtistDetailsResponse artistDetailsResponse = viaplayAlbumService.getArtistDetailsResponse(mbId);
         return new ResponseEntity<ArtistDetailsResponse>(artistDetailsResponse, HttpStatus.OK);
     }
